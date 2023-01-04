97364ed2839724a449bb55ed7a1a872a54b03ed1
Vincenzo Mastromauro <vincenzo@Vincenzos-MacBook-Air.local>
2023-01-04 13:22:42 +0100

    theme toggler redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import ThemeToggler from "./components/themeToggler/themeToggler";
 
 import "./App.css";
 
 function App() {
-  return <h1>test</h1>;
+  const themeColor = useSelector((state: RootState) => {
+    return state.theme.themeColor;
+  });
+
+  return (
+    <div className={`${themeColor ? "lightTheme" : "darkTheme"}`}>
+      <ThemeToggler />
+      <h1>test</h1>
+    </div>
+  );
 }
 
 export default App;
