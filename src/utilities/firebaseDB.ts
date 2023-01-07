4ea84b8722fab20ab605ef6c09883e22a0f752f3
Vincenzo Mastromauro <vincenzo@Vincenzos-MacBook-Air.local>
2023-01-07 19:13:30 +0100

    database file changed

--- src/utilities/firebaseDB.ts
+++ src/utilities/firebaseDB.ts
@@ -1,16 +1,21 @@
 // add to db - remove from db - get from db
-import { getDatabase, ref, update, remove, onValue } from "firebase/database";
+import { getDatabase, ref, update, remove, onValue, set } from "firebase/database";
 import { ShowType } from "../api/_service";
 import { app } from "./firebase";
 
-//using local storage
-
-const db = getDatabase(app);
+export const writeUser = (uid: string, email: string) => {
+  const db = getDatabase(app);
+  set(ref(db, "users/" + uid), {
+    email: email,
+  });
+};
 
 // add to db
 export const addFavorite = (uid: string, data: ShowType) => {
+  const db = getDatabase(app);
+
   const showID = data.id;
-  //checks if the imgage is null or not
+  //checks if the image is null or not
   if (typeof data.image === "undefined") {
     //if image is undefined
     data.image = "https://picsum.photos/200/300";
@@ -19,11 +24,15 @@ export const addFavorite = (uid: string, data: ShowType) => {
 };
 // remove to db
 export const removeFavorite = (uid: string, data: ShowType) => {
+  const db = getDatabase(app);
+
   const showID = data.id;
   remove(ref(db, `users/${uid}/shows/${showID}`));
 };
 //get from db
 export const takeFavorites = (uid: string, callback: any) => {
+  const db = getDatabase(app);
+
   onValue(ref(db, `users/${uid}/shows`), (snapshot) => {
     const data = snapshot.val();
     callback(data);
